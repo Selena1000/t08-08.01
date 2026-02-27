047e653ade6bce2c0ff83b6c0a7c4df06e9c7e18
Selena <seab1000@stud.ek.dk>
2026-02-27 08:55:56 +0100

    js update

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
-const categoryContainer = document.querySelector("#categorylist ");
+const categoryContainer = document.querySelector("#categorylist");
 console.log(categoryContainer);
 
-const categories = ["Accessories", "Apparel", "Footwear", "Free Items", "Personal Care", "Sporting Goods"];
+// const categories = ["Accessories", "Apparel", "Footwear", "Free Items", "Personal Care", "Sporting Goods"];
 
-console.log("category: ", categories[3]);
+// console.log("category: ", categories[3]);
 
 fetch("https://kea-alt-del.dk/t7/api/categories")
   .then((response) => response.json())
   .then((data) => console.log(data));
+
+categories.forEach((category) => {
+  categoryContainer.innerHTML += `<a href="productlist.html">${category.category}</a>`;
+});
